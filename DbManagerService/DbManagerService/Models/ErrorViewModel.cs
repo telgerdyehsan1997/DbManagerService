8eddd1ff1e35907458661ca0c06370e037c65d69
Ehsan Telgerdy <ehsantelgerdybb1@gmail.com>
2020-02-13 15:54:07 +0330

    Project Created

---
+++ DbManagerService/DbManagerService/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DbManagerService.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
